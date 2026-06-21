5acd6d2e38532eee53d239db192cbba7c8d4904c
HassanAlsurh <HassanAlsurh@gmail.com>
2026-06-21 15:38:32 +0300

    Mute and Next Methods

--- app.js
+++ app.js
@@ -1,5 +1,41 @@
-const Person = {
-    Name: 'Ben'
-}
+// const Person = {
+//     Name: 'Ben'
+// }
 
-console.log(Person.Name)
+
+// Person.Name = 'Hasan'
+
+
+// //console.log(Person.Name)
+
+// const MyPlayList = {
+//     Name: ['X1','X2','X3','x4']
+// } 
+
+// console.log(MyPlayList.Name[1])
+
+// delete MyPlayList.Name
+
+// console.log(MyPlayList)
+
+const music = {
+  currentTrack: 'Just Ken',
+  trackIdx: 0,
+  currentPlaylist: ['Just Ken', 'Hey Blondie', 'What Was I Made For', 'Dance The Night'],
+  volume: 70,
+  mute() {
+    music.volume = 0;
+  },
+  next() {
+    music.trackIdx += 1;
+    music.currentTrack = music.currentPlaylist[music.trackIdx]
+  },
+
+};
+
+console.log(music.currentTrack +' '+music.trackIdx+' '+music.volume)
+
+music.next()
+music.mute()
+
+console.log(music.currentTrack +' '+music.trackIdx+' '+music.volume)
